ea94ef40476c89b2ac1f084eefff530c22016e55
Libia Perez <alexandra220790@gmail.com>
2021-09-04 20:16:28 -0300

    Agregando reset.scss, normalize.scss, fonts.scss, arreglando bugs con la carga de imagenes para desktop, paddings, tamaño de fuentes, input y textares del formulario

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView:1,
-  spaceBetween:30,
-  loop:true,
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -11,3 +11,184 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+// Google maps
+function initMap() {
+  const zoom = 16.8
+  const src = '../assets/icons/marker.svg'
+  const lat = '-34.56970433904818'
+  const lng = '-58.42319818866217'
+  const url = 'https://goo.gl/maps/QJ6FPPMYhuJdxL6u5'
+  const center = {
+    lat: parseFloat(lat),
+    lng: parseFloat(lng)
+  }
+  const bounds = new google.maps.LatLngBounds()
+  const map = new google.maps.Map(document.getElementById('map'), {
+    styles,
+    zoom,
+    center,
+    gestureHandling: 'cooperative',
+    disableDefaultUI: true,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: true
+  })
+
+  const latLng = new google.maps.LatLng(lat, lng)
+  const marker = new google.maps.Marker({
+    position: latLng,
+    map,
+    icon: src
+  })
+
+  google.maps.event.addListener(marker, 'click', (function () {
+    window.open(url)
+  }))
+
+  bounds.extend(latLng)
+
+  // Marker to fit zoom
+  map.panTo(center)
+}
+
+const styles = [
+  {
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      },
+      {
+        "lightness": 33
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f7f7f7"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#deecdb"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {
+        "lightness": "25"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "saturation": "-90"
+      },
+      {
+        "lightness": "25"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#e0f1f9"
+      }
+    ]
+  }
+]
